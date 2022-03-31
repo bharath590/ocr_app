5d6e9ef8b156fbb2b6ff09e04839fbc89584872b
bharath <chinnabalu590@gmail.com>
2022-03-31 15:33:02 -0500

    ocr project

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Upload from './Upload';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<Upload />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
